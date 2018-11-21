caec2f20bef2f78638855ee39dd8ee7f255ab080
Mohab999 <mohab@gmail.com>
2018-11-21 20:30:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1";
+var prefix = "3";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "514853712867491910"; // ايدي السررفر
+var channel = "514853712871686144";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('**H-§-N , H-§-N , H-§-N , H-§-N , H-§-N , H-§-N , H-§-N , H-§-N , H-§-N , H-§-N , H-§-N , H-§-N , H-§-N , H-§-N , H-§-N , H-§-N , H-§-N , H-§-N , **')
     },305);
 })
 
